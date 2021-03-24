fa73cfcbc1b0c4a28da97ac5c2f976ea512d16f9
Neidson Souza <neidson.souza@wisereducacao.com>
2021-03-24 17:43:24 -0300

    test_main not created

--- main.py
+++ main.py
@@ -4,45 +4,65 @@ from classes.Certificate import Certificate
 from classes.Gmail import Gmail
 from classes.File import File
 from classes.EmailMessage import EmailMessage
-from dotenv import load_dotenv
 
-load_dotenv()
-os.environ['APP_ROOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
-os.environ['LETSENCRYPT_DIR'] = os.environ.get('APP_ROOT_DIR') + '/letsencrypt/'
-os.chdir(os.environ.get('APP_ROOT_DIR'))
 
-domains_file = File('domains')
-domains = domains_file.get_content_as_list()
+def get_domains_as_class():
+    domains_file = File('domains')
+    domains = domains_file.get_content_as_list_of_class()
+    return domains
 
-domains_fails = []
-should_commit = False
-for domain in domains:
-    certificate = Certificate(domain.name, domain.owner, REPOSITORY_DIR)
-    if certificate.exists():
-        if certificate.is_close_to_expire():
-            certificate.create()
-    else:
-        certificate.create()
-        should_commit = True
-    if certificate.is_close_to_expire():
-        domains_fails.append(domain.name)
 
-if should_commit:
-    os.system('git add *.pem')
-    os.system("git commit -m'[skip ci] Adding certs'")
-    os.system("git push")
+def create_certs_if_expired(domains):
+    global should_commit = False
+    for domain in domains:
+        cert = Certificate(domain.name, domain.owner)
+        if cert.exists():
+            if cert.is_close_to_expire():
+                cert.create()
+        else:
+            cert.create()
+            should_commit = True
 
-if len(domains_fails) > 0:
-    sent_from = 'infra.edtech@wisereducacao.com'
-    to = File('emails').read_file()
-    subject = "[WARNING] Falha ao gerar certificados"
-    email = EmailMessage(domains_fails)
-    message = email.create_email_message(sent_from, to, subject)
 
-    print(message)
+def get_domains_that_fail(domains):
+    domains_that_fail = []
+    for domain in domains:
+        if cert.is_close_to_expire():
+            domains_that_fail.append(domain.name)
+    return domains_that_fail
+
+
+def git_push(should_commit):
+    if should_commit:
+        os.system('git add *.pem')
+        os.system("git commit -m'[skip ci] Adding certs'")
+        os.system("git push")
+
+
+def get_email_message(domains_that_fail):
+    if len(domains_that_fail) > 0:
+        sent_from = 'infra.edtech@wisereducacao.com'
+        to = File('emails').read_file()
+        subject = "[WARNING] Falha ao gerar certificados"
+        email = EmailMessage(domains_that_fail)
+        message = email.create_email_message(sent_from, to, subject)
+        print(message)
+        return message
+
+
+def send_email(message):
     GMAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD')
     gmail_account = Gmail(
         'infra.edtech@wisereducacao.com', GMAIL_PASSWORD
-        )
+    )
     gmail_account.server.sendmail(sent_from, to, message)
     gmail_account.server.close()
+
+
+if __name__ == '__main__':
+    domains = get_domains_as_class()
+    create_certs_if_expired(domains)
+    domains_that_fail = get_domains_that_fail(domains)
+    message = get_email_message(domains_that_fail)
+    send_email(message)
+    git_push(should_commit)
