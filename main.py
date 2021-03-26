75df23b255832492ed9bf25d992f2761597a0195
Neidson Souza <neidson.souza@wisereducacao.com>
2021-03-26 19:08:36 -0300

    creating test main

--- main.py
+++ main.py
@@ -1,76 +1,43 @@
-import os
 import sys
-from classes.Certificate import Certificate
-from classes.Gmail import Gmail
 from classes.File import File
-from classes.EmailMessage import EmailMessage
 
 
-def get_domains_as_class():
-    domains_file = File('domains')
-    domains = domains_file.get_content_as_list_of_class()
-    return domains
+def main(flag):
+    if len(sys.argv) != 1:
+        raise_error()
+    elif flag == '--list-certs':
+        list_certs()
+    elif flag == '--upgrade-certs-repository':
+        pass
+    elif flag == '--upgrade-certs-prod':
+        pass
+    else:
+        raise_error()
 
 
-def create_certs_if_expired_or_inexistent(domains):
-    global should_commit
-    should_commit = False
-    for domain in domains:
-        cert = Certificate(domain.name, domain.owner)
-        if cert.exists():
-            if cert.is_close_to_expire():
-                cert.create()
-        else:
-            cert.create()
-            should_commit = True
+def raise_error():
+    raise ValueError(
+        """
+
+Flags available:
+
+--list-certs: only list certs and their expiry dates
+--upgrade-certs-repository: upgrade certs only in repository (not in production)
+--upgrade-certs-prod: upgrade certs in production (BE CAREFULL !!!)
+"""
+    )
 
 
-def get_domains_that_fail(domains):
-    domains_that_fail = []
-    for domain in domains:
-        cert = Certificate(domain.name, domain.owner)
-        if cert.exists():
-            if cert.is_close_to_expire():
-                domains_that_fail.append(domain.name)
-        else:
-            domains_that_fail.append(domain.name)
-    return domains_that_fail
-
-
-def git_push(should_commit):
-    if should_commit:
-        os.system('git add *.pem')
-        os.system("git commit -m'[skip ci] Adding certs'")
-        os.system("git push")
-
-
-def get_email_message(domains_that_fail):
-    if len(domains_that_fail) > 0:
-        sent_from = 'infra.edtech@wisereducacao.com'
-        to = File('emails').get_data_from_file()
-        subject = "[WARNING] Falha ao gerar certificados"
-        email = EmailMessage(domains_that_fail)
-        message = email.create_email_message(sent_from, to, subject)
-        print(message)
-        return message
-
-
-def send_email(message):
-    if isinstance(message, str):
-        GMAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD')
-        gmail_account = Gmail(
-            'infra.edtech@wisereducacao.com', GMAIL_PASSWORD
-        )
-        sent_from = 'infra.edtech@wisereducacao.com'
-        to = File('emails').get_data_from_file()
-        gmail_account.server.sendmail(sent_from, to, message)
-        gmail_account.server.close()
+def list_certs():
+    domains_file
+    #read file
+    #for em cada item
+        # print cada item em ordem de data (menor para maior)
+        
+    #     domains_file = File('domains')
+    # domains = domains_file.get_content_as_list_of_class()
+    # return domains
 
 
 if __name__ == '__main__':
-    domains = get_domains_as_class()
-    create_certs_if_expired_or_inexistent(domains)
-    domains_that_fail = get_domains_that_fail(domains)
-    message = get_email_message(domains_that_fail)
-    send_email(message)
-    git_push(should_commit)
+    main(sys.argv)
--- test_main.py
+++ test_main.py
@@ -1,65 +1,32 @@
-from collections import namedtuple
+from main import list_cert_dates
 import os
-import pytest
-
-from functions.functions import get_cert_before_creation
-from classes.Certificate import Certificate
-from main import get_domains_as_class
-from main import create_certs_if_expired_or_inexistent
-from main import get_domains_that_fail
-from main import get_email_message
 
 
-@pytest.fixture
-def get_class_domain():
-    return namedtuple('Domain', ['name', 'owner'])
+def test_main():
+    os.system(
+        """\
+echo 'meusucesso.com,34.120.42.5,cloudflare
+wiseup.com,34.95.76.197,cloudflare' > file
+"""
+    )
+    expected = """\
+{:20} - Expiry date: {}
+{:20} - Expiry date: {}
+""".format(
+        'wiseup.com', 'April 06 2020 - 03:12:36',
+        'meusucesso.com', 'April 12 2020 - 03:12:36'
+    )
+    sys_argv = ['--list-certs']
+
+    main(sys_argv)
+    captured = capsys.readouterr()
+    assert captured.out == expected
 
 
-def test_get_domains_as_class(get_class_domain):
-    os.system('echo  "wiserpv.com=cloudflare\ntest.com=aws" > domains')
-    Domain = get_class_domain
-
-    actual = get_domains_as_class()
-    expected = [Domain('wiserpv.com', 'cloudflare'), Domain('test.com', 'aws')]
-    assert expected == actual
 
 
-@pytest.fixture
-def cert_before_creation():
-    cert = Certificate('wiserpv.com', 'cloudflare')
-    return get_cert_before_creation(cert)
 
 
-def test_create_certs_if_expired_or_inexistent(cert_before_creation, get_class_domain):
-    Domain = get_class_domain
-    domains = [Domain('wiserpv.com', 'cloudflare')]
-    create_certs_if_expired_or_inexistent(domains)
-    assert cert_before_creation.exists() == True
 
-
-def test_get_domains_that_fail_empty(cert_before_creation, get_class_domain):
-    cert_before_creation.create()
-    Domain = get_class_domain
-    domains = [Domain('wiserpv.com', 'cloudflare')]
-    actual = get_domains_that_fail(domains)
-    expected = []
-    assert expected == actual
-
-
-def test_get_domains_that_fail_not_empty(cert_before_creation, get_class_domain):
-    Domain = get_class_domain
-    domains = [Domain('wiserpv.com', 'cloudflare')]
-    actual = get_domains_that_fail(domains)
-    expected = ['wiserpv.com']
-    assert expected == actual
-
-
-def test_get_email_message_not_empty():
-    message = get_email_message(['wiserpv.com'])
-    assert isinstance(message, str)
-    assert len(message) != 0
-
-
-def test_get_email_message_empty():
-    message = get_email_message([])
-    assert message == None
+    # criar test passando mais de um argumento
+    # criar testes passando mais de um paramentro para a função man
