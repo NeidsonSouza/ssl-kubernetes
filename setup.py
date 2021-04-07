0628f6598ae460766bd6ad3111e054713c0d28c4
Neidson Souza <neidson.souza@wisereducacao.com>
2021-04-07 16:09:42 -0300

    fix bug setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="sslautomation",
     packages=find_packages(),
     install_requires=[
+        'pyOpenSSL>=20.0.1,<=20.0.1',
         'certbot-dns-cloudflare>=1.13.0,<=1.13.0'
     ]
 )
