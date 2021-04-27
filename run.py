3f929bb761d185835660cda6ade4a33547201cb1
Neidson Souza <neidson.souza@wisereducacao.com>
2021-04-27 18:05:58 -0300

    test new format json

--- run.py
+++ run.py
@@ -12,22 +12,25 @@ if __name__ == '__main__':
         'array':
             [
                 {
+                    '5_days_or_less_to_expiry': True,
                     'runtime': '2012-04-23T18:25:43.511Z',
                     'domain': 'meusucesso.com',
                     'expiry_date:': '2021-06-23T18:25:43.511Z',
-                    'was_updated': True
+                    'was_cert_replaced': True
                 },
                 {
+                    '5_days_or_less_to_expiry': True,
                     'runtime': '2012-04-23T18:25:43.511Z',
                     'domain': 'wiseup.com',
                     'expiry_date:': '2020-05-11T17:19:43.511Z',
-                    'was_updated': False
+                    'was_cert_replaced': False
                 },
                 {
+                    '5_days_or_less_to_expiry': False,
                     'runtime': '2012-04-23T18:25:43.511Z',
                     'domain': 'powerhouse.pro',
                     'expiry_date:': '2022-11-02T15:25:08.511Z',
-                    'was_updated': True
+                    'was_cert_replaced': True
                 }
             ]
     }
