aee28fe874a7bc23f37a18c859f7f14408971aa6
Neidson Souza <neidson.souza@wisereducacao.com>
2021-05-19 11:57:57 -0300

    aws included

--- sslautomation/LocalCert.py
+++ sslautomation/LocalCert.py
@@ -25,17 +25,30 @@ class LocalCert(Certificate):
     def create(self):
         os.makedirs('/tmp/letsencrypt/{}'.format(self.domain))
         print(os.getenv('SERVER'))
-        os.system(
-            """certbot certonly \
-            --non-interactive \
-            --email dominios@wisereducacao.com \
-            --server $SERVER \
-            --dns-cloudflare \
-            --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
-            --dns-cloudflare-propagation-seconds 120 \
-            --agree-tos \
-            -d {0},*.{0}""".format(self.domain)
-        )
+        if self.domain_manager == 'cloudflare':
+            os.system(
+                """certbot certonly \
+                --non-interactive \
+                --email dominios@wisereducacao.com \
+                --server $SERVER \
+                --dns-cloudflare \
+                --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
+                --dns-cloudflare-propagation-seconds 120 \
+                --agree-tos \
+                -d {0},*.{0}""".format(self.domain)
+            )
+        elif self.domain_manager == 'aws':
+            os.system(
+                """certbot certonly \
+                --non-interactive \
+                --email dominios@wisereducacao.com \
+                --server $SERVER \
+                --dns-route53 \
+                --agree-tos \
+                -d {0},*.{0}""".format(self.domain)
+            )
+        else:
+            raise ValueError("ERROR: {} not found in rules. It shoud be 'aws' or 'cloudflare'")
         
     
     def get_expiry_date(self):
